2c5b197e673be1b11edb7f77493a7b9fc6f206ab
vohongminh <74746457+vohongminh@users.noreply.github.com>
2026-04-23 10:10:03 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>TNV iSolutions 🚀</h1>
+      <p>Deploy thành công trên Vercel</p>
+
+      <a href="/login">Go to Login</a>
+    </main>
+  );
+}
